900f98220bb35ff2221e3ff37f5cfb30d3e89235
Yacine Djenidi <djenidi.dev@gmail.com>
2024-01-01 22:32:26 +0100

    Update index.js

--- index.js
+++ index.js
@@ -331,7 +331,7 @@ app.get('/detail', async (req, res) => {
           
 
           var shaped = {
-              name: prsd.data.metaDataComponent.title.replace("| |   - AliExpress", ""),
+              name: prsd.data.metaDataComponent.title.replace(" - AliExpress", ""),
               cover: prsd.data.imageComponent.imagePathList[0],
               shipping: shipping(),
               shippingInfo: shippingInfo(),
